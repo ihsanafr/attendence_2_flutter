ea719788237c54744fbe7bdf34bfab2efe257de5
ihsan <ihsan.ahmad2404@gmail.com>
2025-02-04 08:33:51 +0700

    setup firebase

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCI7WHKf8qkRKWWRH6mpsnJIgp_u5AgPYM", //current key
+      appId: "1:209395904434:android:16315b7634eb89aa9ebeab", //mobilesdk_appid
+      messagingSenderId: "209395904434", //Project Number
+      projectId: "attendence-edit", //project id
+    )
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
---
+++ lib/utils/face_detection/google_ml_kit.dart
@@ -0,0 +1,7 @@
+import 'package:attendence_edit/utils/face_detection/vision.dart';
+
+class GoogleMlKit {
+  GoogleMlKit._();
+
+  static final Vision vision = Vision.instance;
+}
