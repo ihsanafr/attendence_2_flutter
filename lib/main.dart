2f2d4867063800d54a382a9784bf2bfd868ccb37
ihsan <ihsan.ahmad2404@gmail.com>
2025-02-05 08:24:43 +0700

    homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:attendence_edit/ui/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/ui/attendence/attendence.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AttendenceScreen extends StatelessWidget {
+  const AttendenceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
